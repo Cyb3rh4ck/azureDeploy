bc91079568efa0a91b0086db8ccad539a3f682a7
Cyb3rh4ck <cyb3rh4ck@outlook.com>
2024-01-26 11:30:21 -0600

    primer commit

---
+++ src/main/java/mx/santander/fatcalogin/FatcaloginServiceApplication.java
@@ -0,0 +1,13 @@
+package mx.santander.fatcalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatcaloginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatcaloginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/santander/fatcalogin/FatcaloginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.santander.fatcalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatcaloginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
